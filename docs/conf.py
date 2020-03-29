52c194653904494a1b14b88693b273cbede5192c
AtilioA <atiliodadalto@hotmail.com>
2020-03-29 18:39:06 -0300

    Adiciona "master_doc = 'index'" ao conf.py para build de docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'Atílio Antônio'
 # The full version, including alpha/beta/rc tags
 release = '1.0.4'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 

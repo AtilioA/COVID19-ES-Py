d44728dbdba3f7781088e230a59405bdf5be9a2a
AtilioA <atiliodadalto@hotmail.com>
2020-03-29 23:22:45 -0300

    Adiciona título, datas, corpo e número de municípios com casos
    Ajusta documentação, atualiza dependências

--- COVID19_ES_Py/boletim.py
+++ COVID19_ES_Py/boletim.py
@@ -7,9 +7,9 @@ import re
 import logging
 import unicodedata
 import requests
-import configparser
+import arrow
 from bs4 import BeautifulSoup
-from .utils import remove_caracteres_especiais
+from .utils import remove_caracteres_especiais, MUNICIPIOS
 from .exceptions import BoletimError
 
 DOMINIO_FEED = "https://coronavirus.es.gov.br/Noticias"
@@ -26,14 +26,14 @@ mainLogger.setLevel(logging.DEBUG)
 
 class ScraperBoletim():
     """
-    Um objeto ScraperBoletim é capaz de varrer o feed de boletins em busca de suas URLs.
+    Um objeto `ScraperBoletim` é capaz de varrer o feed de boletins em busca de suas URLs.
 
     Attributes
     ----------
-    URLFeedBoletins : string
+    URLFeedBoletins : ``str``
         A URL do feed de boletins.
-    URLsBoletins : list : string
-        Uma lista com URLs dos boletins.
+    URLsBoletins : ``list`` : ``str``
+        Uma lista com URLs dos boletins (preenchida caso o método `extrai_todos_boletins` seja chamado).
     """
     def __init__(self, ):
         self.URLFeedBoletins = DOMINIO_FEED
@@ -44,13 +44,13 @@ class ScraperBoletim():
 
         Parameters
         ----------
-        URLPagina : string
+        URLPagina : ``str``
             Página do feed a ser varrido.
 
         Returns
         ----------
-        html : objeto BS4 ou None
-            O objeto BS4 do HTML lido ou None se houver falha na requisição."""
+        html : `BeautifulSoup` ou ``None``
+            O objeto BeautifulSoup do HTML lido ou ``None`` se houver falha na requisição."""
 
         if URLPagina:
             req = requests.get(URLPagina)
@@ -70,12 +70,12 @@ class ScraperBoletim():
 
         Parameters
         ----------
-        URLPagina : string
+        URLPagina : ``str``
             Página do feed a ser varrido.
 
         Returns
         ----------
-        linksBoletins : list : string ou None
+        linksBoletins : ``list`` : ``str`` ou None
             Lista com URLs dos boletins da página do feed ou None se houver falha na requisição."""
 
         try:
@@ -92,7 +92,7 @@ class ScraperBoletim():
 
         Returns
         ----------
-        todosBoletins : list : string
+        todosBoletins : ``list`` : ``str``
             Lista com URLs de todos os boletins publicados até o momento."""
 
         todosBoletins = []
@@ -114,7 +114,7 @@ class ScraperBoletim():
         Returns
         ----------
         url : ``string`` ou ``None``
-            URL do último boletim ou None se busca falhar."""
+            URL do último boletim ou ``None`` se busca falhar."""
 
         html = self.carrega_html_feed(self.URLFeedBoletins)
         try:
@@ -129,8 +129,8 @@ class ScraperBoletim():
 
         Returns
         ----------
-        boletim : Boletim
-            Objeto Boletim carregado com a URL do último boletim emitido."""
+        boletim : `Boletim`
+            Objeto `Boletim` carregado com a URL do último boletim emitido."""
 
         urlBoletim = self.url_ultimo_boletim()
         return Boletim(urlBoletim)
@@ -138,50 +138,110 @@ class ScraperBoletim():
 
 class Boletim():
     """
-    Um objeto Boletim é capaz de extrair números de casos de um boletim.
+    Um objeto `Boletim` é capaz de extrair números de casos de um boletim.
 
     Parameters
     ----------
-    URLBoletim : string
+    URLBoletim : ``str``
         A URL do boletim a ser processado.
 
     Attributes
     ----------
-    url : string
+    url : ``str``
         A URL do boletim.
-    n : string
+    n : ``int``
         O número do boletim.
-    html : objeto BS4
-        O HTML lido como objeto BS4.
-    casos : dict
+    titulo : ``str``
+        Título do boletim.
+    dataPublicacao : objeto ``Arrow``
+        Objeto ``Arrow`` com a data de publicação do boletim.
+    dataAtualizacao : objeto ``Arrow``
+        Objeto ``Arrow`` com a data de atualização do boletim, se houver.
+    corpo : ``str``
+        Corpo de texto do boletim.
+    html : `BeautifulSoup`
+        O HTML lido como objeto BeautifulSoup.
+    casos : ``dict``
         O dicionário de casos registrados nos municípios do ES.
-    totalGeral : dict
+    totalGeral : ``dict``
         O dicionário do total de casos registrados no ES.
-    nMunicipiosInfectados : int
-        O número de municípios infectados.
+    nMunicipiosComCasos : ``int``
+        O número de municípios com algum caso, confirmado ou não.
+    nMunicipiosInfectados : ``int``
+        O número de municípios com algum caso confirmado.
     """
 
     def __init__(self, URLBoletim):
         self.url = URLBoletim
         self.n = self.extrai_numero_boletim()
         self.html = self.carrega_html_boletim()
+        self.titulo = self.extrai_titulo_boletim()
+        datas = self.extrai_datas_boletim()
+        self.dataPublicacao = datas[0]
+        self.dataAtualizacao = datas[1]
+        self.corpo = self.extrai_corpo_boletim()
         self.casos = {}
         self.totalGeral = {}
-        self.nMunicipiosInfectados = 0
         self.carrega_casos_boletim()
+        self.nMunicipiosComCasos = self.conta_municipios_com_casos()
+        self.nMunicipiosInfectados = self.conta_municipios_infectados()
 
     def __str__(self):
-        return f"Boletim nº {self.n}.\nURL: {self.url}\n{self.nMunicipiosInfectados} municípios com possíveis casos.\nTotal ES: {self.totalGeral}"
+        return f"Boletim nº {self.n} - { self.pega_dataPublicacao_formatada()}.\nURL: {self.url}\n{self.nMunicipiosComCasos} municípios com possíveis casos.\nTotal ES: {self.totalGeral}"
+
+    def pega_dataPublicacao_formatada(self):
+        """Formata o objeto Arrow com data de publicação para o formato usual dos boletins."""
+
+        if self.dataPublicacao:
+            return self.dataPublicacao.format('DD/MM/YYYY HH:mm')
+
+    def pega_dataAtualizacao_formatada(self):
+        """Formata o objeto Arrow com data de atualização para o formato usual dos boletins."""
+
+        if self.dataAtualizacao:
+            return self.dataAtualizacao.format('DD/MM/YYYY HH:mm')
 
     def extrai_numero_boletim(self):
-        """Extrai o número do boletim."""
+        """Extrai o número do boletim pela URL."""
 
         buscaPadrao = re.search(r'(\d+)o', self.url)
         if buscaPadrao:
-            return buscaPadrao.group(1)
+            return int(buscaPadrao.group(1))
+
+    def extrai_titulo_boletim(self):
+        """Extrai título do HTML do boletim."""
+
+        tituloBoletim = self.html.find('h2', class_='title-content')
+        return tituloBoletim.text
+
+    def extrai_datas_boletim(self):
+        """Extrai as datas de publicação e atualização (esta se existir) do HTML do boletim."""
+
+        datasBoletim = self.html.find('div', class_='published').text
+
+        patternDatas = r"(\d{1,2}\/\d{1,2}\/\d{4}\s*\w+h\w*)"
+        matchData = re.findall(patternDatas, datasBoletim)
+
+        formatoData = "DD/MM/YYYY H[h]m"
+        dataPublicacao = arrow.get(matchData[0], formatoData)
+
+        try:
+            dataAtualizacao = arrow.get(matchData[1], formatoData)
+        except IndexError:
+            dataAtualizacao = None
+
+        return (dataPublicacao, dataAtualizacao)
+
+    def extrai_corpo_boletim(self):
+        """Extrai o corpo de texto do HTML do boletim."""
+
+        corpo = self.html.find('div', class_='clearfix body-part')
+        stringCorpo = corpo.find('p').text
+
+        return stringCorpo
 
     def carrega_html_boletim(self):
-        """Faz requisição para o boletim e retorna seu HTML como objeto BS4."""
+        """Faz requisição para o boletim e retorna seu HTML como objeto `BeautifulSoup`."""
 
         req = requests.get(self.url)
         if req.status_code == 200:
@@ -200,46 +260,79 @@ class Boletim():
 
     def carrega_casos_boletim(self):
         """Carrega tabela do boletim e extrai número de casos.
-        Preenche nMunicipiosInfectados e os dicionários casos e totalGeral."""
+        Preenche os dicionários `casos` e `totalGeral`."""
 
-        tabela = self.carrega_tabela_boletim()
+        if self.n > 20:
+            tabela = self.carrega_tabela_boletim()
 
-        # Exclui header (primeira linha) e total (última linha)
-        for linha in tabela[1:-1]:
-            dadoslinha = list(
-                map(lambda linha: linha.text, linha.find_all("p")))
-            municipio = dadoslinha[0]
+            # Exclui header (primeira linha) e total (última linha)
+            for linha in tabela[1:-1]:
+                dadoslinha = list(
+                    map(lambda linha: linha.text, linha.find_all("p")))
+                municipio = dadoslinha[0]
 
-            self.casos[municipio] = {
-                'casosConfirmados': unicodedata.normalize("NFKD", dadoslinha[1]).replace(' ', '0'),
-                'casosDescartados': unicodedata.normalize("NFKD", dadoslinha[2]).replace(' ', '0'),
-                'casosSuspeitos': unicodedata.normalize("NFKD", dadoslinha[3]).replace(' ', '0'),
-                'totalCasos': unicodedata.normalize("NFKD", dadoslinha[4]).replace(' ', '0')
+                self.casos[municipio] = {
+                    'casosConfirmados': unicodedata.normalize("NFKD", dadoslinha[1]).replace(' ', '0'),
+                    'casosDescartados': unicodedata.normalize("NFKD", dadoslinha[2]).replace(' ', '0'),
+                    'casosSuspeitos': unicodedata.normalize("NFKD", dadoslinha[3]).replace(' ', '0'),
+                    'totalCasos': unicodedata.normalize("NFKD", dadoslinha[4]).replace(' ', '0')
+                }
+
+            dadosTotalGeral = list(map(lambda linha: unicodedata.normalize(
+                "NFKD", linha.text), tabela[-1].find_all("p")))
+            self.totalGeral = {
+                'casosConfirmados': unicodedata.normalize("NFKD", dadosTotalGeral[1]),
+                'casosDescartados': unicodedata.normalize("NFKD", dadosTotalGeral[2]),
+                'casosSuspeitos': unicodedata.normalize("NFKD", dadosTotalGeral[3]),
+                'totalCasos': unicodedata.normalize("NFKD", dadosTotalGeral[4])
             }
 
-        dadosTotalGeral = list(map(lambda linha: unicodedata.normalize(
-            "NFKD", linha.text), tabela[-1].find_all("p")))
-        self.totalGeral = {
-            'casosConfirmados': unicodedata.normalize("NFKD", dadosTotalGeral[1]),
-            'casosDescartados': unicodedata.normalize("NFKD", dadosTotalGeral[2]),
-            'casosSuspeitos': unicodedata.normalize("NFKD", dadosTotalGeral[3]),
-            'totalCasos': unicodedata.normalize("NFKD", dadosTotalGeral[4])
-        }
+        else:
+            mainLogger.error(f"O boletim de nº {self.n} (n < 21, anterior a 19/03/2020) não é suportado.")
+            raise BoletimError(f"O boletim de nº {self.n} (n < 21, anterior a 19/03/2020) não é suportado.")
 
-        self.nMunicipiosInfectados = len(tabela[1:-1])
+    def conta_municipios_com_casos(self):
+        """Conta o número de municípios com algum caso registrado, confirmado ou não."""
+
+        municipiosComCasos = 0
+        linhasTabela = self.casos.keys()
+
+        for elemento in linhasTabela:
+            if remove_caracteres_especiais(elemento).upper() in MUNICIPIOS:
+                municipiosComCasos += 1
+
+        return municipiosComCasos
+
+    def conta_municipios_infectados(self):
+        """Conta o número de municípios com algum caso confirmado."""
+
+        municipiosInfectados = 0
+        linhasTabela = self.casos.keys()
+
+        for elemento in linhasTabela:
+            elementoTratado = remove_caracteres_especiais(elemento).upper()
+            if elementoTratado in MUNICIPIOS and int(self.casos[elemento]['casosConfirmados']) > 0:
+                municipiosInfectados += 1
+
+        return municipiosInfectados
 
     def pesquisa_casos_municipio(self, municipio):
         """Realiza pesquisa no boletim por casos registrados em um município.
 
         Parameters
         ----------
-        municipio : string
+        municipio : ``str``
             O município a ser pesquisado.
+
+        Raises
+        ----------
+        `BoletimError`
+            Se o município não for encontrado na lista de casos.
 
         Returns
         ----------
-        self.casos[municipio] : dict
-            O dicionário de casos registrados no município, ou `BoletimError` se não for encontrado."""
+        self.casos[municipio] : ``dict``
+            O dicionário de casos registrados no município."""
 
         stringMunicipioTratada = remove_caracteres_especiais(municipio).lower().strip()
         stringsMunicipiosTratadas = list(

f6e40bf1f437e8c43306ed55429a5abac5c7a0d3
AtilioA <atiliodadalto@hotmail.com>
2020-04-18 17:09:11 -0300

    Adiciona avisos de descontinuação

--- COVID19_ES_Py/exceptions.py
+++ COVID19_ES_Py/exceptions.py
@@ -1,6 +1,9 @@
-"""O módulo `exceptions.py` apenas declara as exceções `BoletimError` e `RelatorioError`."""
+"""O módulo `exceptions.py` apenas declara as exceções `BoletimError` (descontinuada) e `RelatorioError`."""
+
+from deprecated import deprecated
 
 
+@deprecated(version='2.0.0', reason="BoletimError foi descontinuado no COVID19-ES-Py 2.0.0, e será substituído por `RelatorioError` na versão 3.0.0 uma vez que a SESA não emitirá mais boletins de COVID19.")
 class BoletimError(Exception):
     pass
 

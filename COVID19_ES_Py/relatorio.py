f2c64a3f6b9cc4d6853cd4a7e11fe864c25ae797
AtilioA <atiliodadalto@hotmail.com>
2020-04-18 17:19:31 -0300

    Cria classe LeitorRelatorio e métodos iniciais
    Estes métodos implementam mais ou menos os métodos das classes descontinuadas ScraperBoletim e Boletim
    
    Adiciona dosctrings restantes e corrige alguns bugs

--- COVID19_ES_Py/relatorio.py
+++ COVID19_ES_Py/relatorio.py
@@ -1,16 +1,18 @@
-"""O módulo `relatorio.py` é o principal do pacote a partir de 14/04/2020.
+"""O módulo `relatorio.py` é o principal do pacote a partir de 14/04/2020 (`COVID19-ES-Py 2.0.0`).
 Nele são introduzidas as classes e métodos utilizados para coletar dados dos relatórios emitidos pelo Governo através do painel PowerBI.
 
 """
 
 from functools import total_ordering
 
+from pathlib import Path
 from io import BytesIO
 import requests
 import rows
 import arrow
 
-from COVID19_ES_Py.utils import MUNICIPIOS, remove_caracteres_especiais, trata_dados_linha
+from .utils import MUNICIPIOS, remove_caracteres_especiais, trata_dados_linha
+from .exceptions import RelatorioError
 
 # URL direta para o arquivo csv dos microdados do painel PowerBI
 URL_RELATORIO_CSV = "https://bi.static.es.gov.br/covid19/MICRODADOS.csv"
@@ -50,6 +52,12 @@ class Municipio():
 
     def __lt__(self, other):
         return (self.nome.lower() < other.nome.lower())
+
+    def __str__(self):
+        return f"Município {self.nome}:\n{self.casosConfirmados} casos confirmados.\n{self.obitos} óbitos."
+
+    def __repr__(self):
+        return f"{{'casosConfirmados': {self.casosConfirmados}, 'obitos': {self.obitos}}}"
 
 
 class Caso():
@@ -139,6 +147,10 @@ class Caso():
         return f"Caso de {self.data} - {self.classificacao} em {self.municipio}"
 
     def carrega_dados_linha(self, linha):
+        """Carrega os dados presentes em uma linha do csv para o objeto Caso.
+        Retorna o objeto Caso preenchido.
+        """
+
         linha = trata_dados_linha(list(linha))
 
         self.data = linha[0]
@@ -173,15 +185,41 @@ class Caso():
         self.viagemBrasil = linha[25]
         self.viagemInternacional = linha[26]
 
+        return self
+
 
 class Relatorio():
+    """
+    Um objeto `Relatorio` é capaz de abstrair um relatório em csv emitido pelo painel PowerBI.
+
+    Parameters
+    ----------
+    caminhoCSV : ``string``
+        Caminho até o arquivo csv. Se não for informado, baixa do PowerBI.
+
+    Attributes
+    ----------
+    csv : ``str``
+        A string com caminho ou URL do arquivo csv.
+    linhasRelatorio : ``list`` : ``Row``
+        A lista de objetos Row lidos do arquivo csv.
+    casosMunicipios : ``dict`` : ``Municipio``
+        O dicionário de objetos Municipio
+    importadosOuIndefinidos : ``dict`` : ``int``
+        O dicionário com total de casos confirmados e óbitos de municípios indefinidos ou de pacientes de fora do ES.
+    totalGeral : ``dict`` : ``int``
+        O dicionário com total de casos confirmados e óbitos no ES.
+    nMunicipiosInfectados : ``int``
+        O número de municípios infectados deste relatório.
+    """
+
     def __init__(self, caminhoCSV=None):
         if caminhoCSV:
-            self.csv = caminhoCSV
-            self.rows = rows.import_from_csv(self.csv)
+            self.csv = Path(caminhoCSV)
+            self.linhasRelatorio = rows.import_from_csv(self.csv)
         else:
             self.csv = URL_RELATORIO_CSV
-            self.rows = rows.import_from_csv(BytesIO(requests.get(self.csv).content))
+            self.linhasRelatorio = None
 
         self.casosMunicipios = {}
         self.inicializa_dicionario_municipios()
@@ -197,16 +235,48 @@ class Relatorio():
         self.nMunicipiosInfectados = 0
 
     def inicializa_dicionario_municipios(self):
+        """Inicializa o dicionário de municípios do Relatorio."""
+
         for municipio in MUNICIPIOS:
             self.casosMunicipios[municipio] = Municipio(municipio)
 
-    def filtra_data(self, data):
-        dataArrow = arrow.get(data, "DD/MM/YYYY")
+    def busca_casos_municipio(self, municipio):
+        """Realiza pesquisa no Relatorio por casos registrados em um município.
 
-        return [caso for caso in self.rows[1:] if dataArrow >= arrow.get(caso[0], "DD/MM/YYYY") and "Confirmados" in caso[1]]
+        Parameters
+        ----------
+        municipio : ``str``
+            O município a ser pesquisado.
+
+        Raises
+        ----------
+        `RelatorioError`
+            Se o município não for encontrado na lista de casos.
+
+        Returns
+        ----------
+        self.casos[municipio] : ``dict``
+            O dicionário de casos registrados no município."""
+
+        stringMunicipioTratada = remove_caracteres_especiais(
+            municipio).upper().strip()
+        try:
+            return self.casosMunicipios[stringMunicipioTratada]
+        except KeyError:
+            raise RelatorioError(
+                f"O município '{municipio}' não foi encontrado no relatório. Pode ter ocorrido um erro de digitação ou o município não registrou casos de COVID-19.")
 
     def popula_relatorio(self):
-        for linha in self.rows:
+        """Preenche o Relatorio com as informações presentes em self.linhasRelatorio."""
+
+        self.totalGeral['casosConfirmados'] = 0
+        self.totalGeral['obitos'] = 0
+        self.inicializa_dicionario_municipios()
+        self.nMunicipiosInfectados = 0
+        self.importadosOuIndefinidos['casosConfirmados'] = 0
+        self.importadosOuIndefinidos['obitos'] = 0
+
+        for linha in self.linhasRelatorio:
             caso = Caso(linha)
             if caso.classificacao == "Confirmados":
                 if remove_caracteres_especiais(caso.municipio.upper()) in MUNICIPIOS:
@@ -224,7 +294,127 @@ class Relatorio():
                         self.importadosOuIndefinidos['obitos'] += 1
 
                 self.totalGeral['casosConfirmados'] += 1
-                print(self)
+        return self
 
     def __str__(self):
         return f"Relatório do arquivo {self.csv}:\nTotal geral: {self.totalGeral}\n{self.nMunicipiosInfectados} municípios infectados."
+
+
+class LeitorRelatorio():
+    """
+    Um objeto `LeitorRelatorio` é capaz de manipular relatórios emitidos pelo painel PowerBI.
+
+    Parameters
+    ----------
+    caminhoCSV : ``string``
+        Caminho até o arquivo csv. Se não for informado, baixa do PowerBI.
+
+    Attributes
+    ----------
+    csv : ``str``
+        A string com caminho ou URL do arquivo csv.
+    linhasRelatorio : ``list`` : ``Row``
+        A lista de objetos Row lidos do arquivo csv.
+    relatorio : ``Relatorio``
+        O objeto Relatorio criado a partir do csv
+        O número de municípios infectados deste relatório.
+    """
+
+    def __init__(self, caminhoCSV=None):
+        self.relatorio = Relatorio()
+        if caminhoCSV:
+            self.csv = Path(caminhoCSV)
+            self.linhasRelatorio = rows.import_from_csv(self.csv)
+            self.relatorio.csv = self.csv
+            self.relatorio.linhasRelatorio = self.linhasRelatorio
+            self.relatorio.popula_relatorio()
+        else:
+            self.csv = URL_RELATORIO_CSV
+
+    def carrega_ultimo_relatorio(self):
+        """Baixa e lê o arquivo csv mais recente do PowerBI."""
+
+        self.relatorio.csv = URL_RELATORIO_CSV
+        self.linhasRelatorio = rows.import_from_csv(
+            BytesIO(requests.get(self.relatorio.csv).content))
+        self.relatorio.linhasRelatorio = self.linhasRelatorio
+
+        return self.relatorio.popula_relatorio()
+
+    def filtra_casos_ate_dia(self, data):
+        """Filtra relatório por casos até o dia fornecido (incluso).
+
+        Parameters
+        ----------
+        data : ``str``
+            A data de publicação do boletim a ser pesquisado.
+            Formatos de data aceitos:
+            "DD/MM/YYYY", "DD-MM-YYYY", "DD_MM_YYYY", "DD.MM.YYYY", "DDMMYYYY".
+
+        Returns
+        ----------
+        Relatorio : `Relatorio`
+            O Relatorio filtrado e preenchido com os registros até a data especificada.
+        """
+
+        if self.relatorio.linhasRelatorio:
+            dataArrow = arrow.get(
+                data, ["DD/MM/YYYY", "DD-MM-YYYY", "DD_MM_YYYY", "DD.MM.YYYY", "DDMMYYYY"]
+            )
+            self.relatorio.linhasRelatorio = [
+                caso for caso in self.linhasRelatorio[1:] if dataArrow >= arrow.get(
+                    caso[0], "DD/MM/YYYY") and "Confirmados" in caso[1]
+            ]
+            return self.relatorio.popula_relatorio()
+        else:
+            raise RelatorioError(
+                "Não é possível filtrar pois o relatório está vazio (use o método popula_relatorio() para preencher o relatório).")
+
+    def filtra_casos_no_dia(self, data):
+        """Filtra relatório por casos no dia fornecido.
+
+        Parameters
+        ----------
+        data : ``str``
+            A data de publicação do boletim a ser pesquisado.
+            Formatos de data aceitos:
+            "DD/MM/YYYY", "DD-MM-YYYY", "DD_MM_YYYY", "DD.MM.YYYY", "DDMMYYYY".
+
+        Returns
+        ----------
+        Relatorio : `Relatorio`
+            O Relatorio filtrado e preenchido com os registros da data especificada.
+        """
+
+        if self.relatorio.linhasRelatorio:
+            dataArrow = arrow.get(
+                data, ["DD/MM/YYYY", "DD-MM-YYYY", "DD_MM_YYYY", "DD.MM.YYYY", "DDMMYYYY"]
+            )
+            self.relatorio.linhasRelatorio = [
+                caso for caso in self.linhasRelatorio[1:] if dataArrow == arrow.get(
+                    caso[0], "DD/MM/YYYY") and "Confirmados" in caso[1]
+            ]
+            return self.relatorio.popula_relatorio()
+        else:
+            raise RelatorioError(
+                "Não é possível filtrar pois o relatório está vazio (use o método popula_relatorio() para preencher o relatório).")
+
+    def __str__(self):
+        if self.csv:
+            return f"Leitor de relatórios carregado com {self.csv}."
+        else:
+            return f"Leitor de relatórios sem dados para ler."
+
+
+leitor = LeitorRelatorio()
+ultimoRelatorio = leitor.carrega_ultimo_relatorio()
+print(ultimoRelatorio)
+
+# print(ultimoRelatorio.busca_casos_municipio("Vitória"))
+# print(ultimoRelatorio.busca_casos_municipio("  santa teresa "))
+# print(ultimoRelatorio.busca_casos_municipio("AFONSO CLAUDIO"))
+
+# relatorio29_03 = leitor.filtra_casos_ate_dia("17.04.2020")
+# print(relatorio29_03)
+# relatorio29_03 = leitor.filtra_casos_no_dia("16-04-2020")
+# print(relatorio29_03)

f5dcdcd76939f9c5a4b81da40a7e7c130107f0e2
AtilioA <atiliodadalto@hotmail.com>
2020-04-18 12:47:09 -0300

    Corrige uso de índices e começa algumas docstrings
    Modulariza trata_dados_linha

--- COVID19_ES_Py/parse_relatorio_powerbi.py
+++ COVID19_ES_Py/parse_relatorio_powerbi.py
@@ -1,3 +1,8 @@
+"""O módulo `relatorio.py` é o principal do pacote a partir de 14/04/2020.
+Nele são introduzidas as classes e métodos utilizados para coletar dados dos relatórios emitidos pelo Governo através do painel PowerBI.
+
+"""
+
 from functools import total_ordering
 
 from io import BytesIO
@@ -5,49 +10,41 @@ import requests
 import rows
 import arrow
 
-from COVID19_ES_Py.utils import MUNICIPIOS, remove_caracteres_especiais
+from COVID19_ES_Py.utils import MUNICIPIOS, remove_caracteres_especiais, trata_dados_linha
 
+# URL direta para o arquivo csv dos microdados do painel PowerBI
 URL_RELATORIO_CSV = "https://bi.static.es.gov.br/covid19/MICRODADOS.csv"
-
-
-def trata_dados_linha(linha):
-    linha[0] = arrow.get(linha[0], 'DD/MM/YYYY')
-
-    if linha[2] in ["Ignorado", "-"]:
-        linha[2] = None
-    if "-" in linha[3]:
-        linha[3] = None
-    if linha[6] in ["Não encontrado", "NULL"]:
-        linha[6] = None
-    if "Ignorado" in linha[9]:
-        linha[9] = None
-    if "Ignorado" in linha[10]:
-        linha[10] = None
-    if "-" in linha[11]:
-        linha[11] = None
-
-    stringParaBool = {
-        "Sim": True,
-        "Não": False,
-        "-": None,
-        "": None,
-        1: True,
-        2: None,
-    }
-    for i, campo in enumerate(linha[12:]):
-        linha[i + 12] = stringParaBool.get(linha[i + 12], None)
-
-    return linha
 
 
 @total_ordering
 class Municipio():
+    """
+    Um objeto `Municipio` representa um município do estado do Espírito Santo.
+
+    Parameters
+    ----------
+    Nome : ``str``
+        O nome do município a ser atribuído ao objeto.
+
+    Attributes
+    ----------
+    nome : ``str``
+        O nome do município.
+    casos : ``list`` : ``Caso``
+        Uma lista de objetos do tipo Caso.
+    casosConfirmados : ``int``
+        O número de casos confirmados no município.
+    obitos : ``int``
+        O número de óbitos confirmados em decorrência de COVID-19 no município.
+    """
+
     def __init__(self, nome):
         self.nome = nome
         self.casos = []
         self.casosConfirmados = 0
         self.obitos = 0
 
+    # Objetos do tipo Municipio podem ser comparados alfabeticamente
     def __eq__(self, other):
         return (self.nome.lower() == other.nome.lower())
 
@@ -56,19 +53,63 @@ class Municipio():
 
 
 class Caso():
+    """
+    Um objeto `Caso` é capaz de abstrair o registro de um caso lido do csv.
+
+    Parameters
+    ----------
+    dados : ``iterable``
+        Um iterável com os dados do caso.
+
+    Attributes
+    ----------
+    data : Objeto `arrow`
+        A data de registro do caso.
+    classificacao : ``str``
+        A classificação da ocorrência de COVID-19 no paciente.
+    evolucao : ``str`` ou ``None``
+        A evolução da situação do paciente ou None se não for informada.
+    criterioConfirmacao : ``str`` ou ``None``
+        O critério usado para a classificação do caso ou None se não for informada.
+    statusNotificacao : ``str``
+        O estado atual do caso.
+    municipio : ``str``
+        O município de origem do paciente.
+    bairro : ``str`` ou ``None``
+        O bairro de origem do paciente.
+    faixaEtaria : ``str``
+        A faixa etária do paciente.
+    sexo : ``str``
+        O sexo do paciente.
+    racaCor : ``str`` ou ``None``
+        A raça/cor do paciente ou None se não for informada.
+    escolaridade : ``str``
+        O grau de escolaridade do paciente ou ``None`` se não for informada.
+    sintomas : ``dict`` : ``bool``
+        Os sintomas apresentados pelo paciente.
+    comorbidades : ``dict`` : ``bool``
+        As comorbidades apresentadas pelo paciente.
+    ficouInternado : ``bool`` ou ``None``
+        Se o paciente ficou internado ou não.
+    viagemBrasil : ``bool`` ou ``None``
+        Se o paciente realizou viagem nacional ou não.
+    viagemInternacional : ``bool`` ou ``None``
+        Se o paciente realizou viagem internacional ou não.
+    """
+
     def __init__(self,
                  dados=None,
                  data=None,
+                 classificacao=None,
+                 evolucao=None,
+                 criterioConfirmacao=None,
+                 statusNotificacao=None,
+                 municipio=None,
+                 bairro=None,
                  faixaEtaria=None,
                  sexo=None,
                  racaCor=None,
                  escolaridade=None,
-                 classificacao=None,
-                 evolucao=None,
-                 criterioClassificacao=None,
-                 statusNotificacao=None,
-                 municipio=None,
-                 bairro=None,
                  sintomas=None,
                  comorbidades=None,
                  ficouInternado=None,
@@ -80,7 +121,7 @@ class Caso():
             self.data = data
             self.classificacao = classificacao
             self.evolucao = evolucao
-            self.criterioClassificacao = criterioClassificacao
+            self.criterioConfirmacao = criterioConfirmacao
             self.statusNotificacao = statusNotificacao
             self.municipio = municipio
             self.bairro = bairro
@@ -103,7 +144,7 @@ class Caso():
         self.data = linha[0]
         self.classificacao = linha[1]
         self.evolucao = linha[2]
-        self.criterioClassificacao = linha[3]
+        self.criterioConfirmacao = linha[3]
         self.statusNotificacao = linha[4]
         self.municipio = linha[5]
         self.bairro = linha[6]
@@ -112,19 +153,19 @@ class Caso():
         self.racaCor = linha[9]
         self.escolaridade = linha[10]
         self.sintomas = {
-            "cefaleia": linha[11],
-            "coriza": linha[12],
-            "diarreia": linha[13],
-            "dificuldadeRespiratoria": linha[14],
+            "febre": linha[11],
+            "dificuldadeRespiratoria": linha[12],
+            "tosse": linha[13],
+            "coriza": linha[14],
             "dorGarganta": linha[15],
-            "febre": linha[16],
-            "tosse": linha[17]
+            "diarreia": linha[16],
+            "cefaleia": linha[17],
         }
         self.comorbidades = {
-            "comorbidadeCardio": linha[18],
-            "comorbidadeDiabetes": linha[19],
-            "comorbidadePulmao": linha[20],
-            "comorbidadeRenal": linha[21],
+            "comorbidadePulmao": linha[18],
+            "comorbidadeCardio": linha[19],
+            "comorbidadeRenal": linha[20],
+            "comorbidadeDiabetes": linha[21],
             "comorbidadeTabagismo": linha[22],
             "comorbidadeObesidade": linha[23]
         }
@@ -187,5 +228,3 @@ class Relatorio():
 
     def __str__(self):
         return f"Relatório do arquivo {self.csv}:\nTotal geral: {self.totalGeral}\n{self.nMunicipiosInfectados} municípios infectados."
-
-

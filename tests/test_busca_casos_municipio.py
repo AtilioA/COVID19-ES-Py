eb2b6c01e91a69cffadcbda1087cb58538f48259
AtilioA <atiliodadalto@hotmail.com>
2020-05-04 12:32:37 -0300

    Corrige e atualiza test_busca_casos_municipio

--- tests/test_busca_casos_municipio.py
+++ tests/test_busca_casos_municipio.py
@@ -3,76 +3,63 @@ import pytest
 from COVID19_ES_Py import Relatorio, exceptions
 
 
-def test_18_04():
+def test_30_04():
     relatorio = Relatorio(
-        Path("tests/relatorios_passados/18-04-2020.csv"))
+        Path("tests/relatorios_passados/30-04-2020.csv"))
     relatorio.popula_relatorio()
 
-    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 244)
-    assert(relatorio.busca_casos_municipio("Vitória").obitos == 9)
+    print(relatorio)
 
-    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 4)
+    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 554)
+    assert(relatorio.busca_casos_municipio("Vitória").obitos == 16)
+
+    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 14)
     assert(relatorio.busca_casos_municipio("  santa teresa ").obitos == 0)
 
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 2)
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 0)
+    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 10)
+    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 2)
 
     assert(relatorio.busca_casos_municipio("vILA pavao").casosConfirmados == 0)
     assert(relatorio.busca_casos_municipio("vILA pavao").obitos == 0)
 
 
-def test_20_04():
-    pass
+def test_02_04():
     relatorio = Relatorio(
-        Path("tests/relatorios_passados/20-04-2020.csv"))
+        Path("tests/relatorios_passados/02-05-2020.csv"))
     relatorio.popula_relatorio()
 
-    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 265)
-    assert(relatorio.busca_casos_municipio("Vitória").obitos == 9)
+    print(relatorio)
 
-    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 4)
+    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 621)
+    assert(relatorio.busca_casos_municipio("Vitória").obitos == 18)
+
+    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 17)
     assert(relatorio.busca_casos_municipio("  santa teresa ").obitos == 0)
 
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 2)
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 0)
+    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 11)
+    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 2)
 
     assert(relatorio.busca_casos_municipio("vILA pavao").casosConfirmados == 0)
     assert(relatorio.busca_casos_municipio("vILA pavao").obitos == 0)
 
 
-def test_21_04():
+def test_03_04():
     relatorio = Relatorio(
-        Path("tests/relatorios_passados/21-04-2020.csv"))
+        Path("tests/relatorios_passados/03-05-2020.csv"))
     relatorio.popula_relatorio()
 
-    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 290)
-    assert(relatorio.busca_casos_municipio("Vitória").obitos == 9)
+    print(relatorio)
 
-    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 4)
+    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 628)
+    assert(relatorio.busca_casos_municipio("Vitória").obitos == 20)
+
+    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 17)
     assert(relatorio.busca_casos_municipio("  santa teresa ").obitos == 0)
 
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 2)
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 0)
+    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 13)
+    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 2)
 
-    assert(relatorio.busca_casos_municipio("vILA pavao").obitos == 0)
-    assert(relatorio.busca_casos_municipio("vILA pavao").obitos == 0)
-
-
-def test_22_04():
-    relatorio = Relatorio(
-        Path("tests/relatorios_passados/22-04-2020.csv"))
-    relatorio.popula_relatorio()
-
-    assert(relatorio.busca_casos_municipio("Vitória").casosConfirmados == 291)
-    assert(relatorio.busca_casos_municipio("Vitória").obitos == 9)
-
-    assert(relatorio.busca_casos_municipio("  santa teresa ").casosConfirmados == 4)
-    assert(relatorio.busca_casos_municipio("  santa teresa ").obitos == 0)
-
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").casosConfirmados == 2)
-    assert(relatorio.busca_casos_municipio("AFONSO CLAUDIO").obitos == 0)
-
-    assert(relatorio.busca_casos_municipio("vILA pavao").obitos == 0)
+    assert(relatorio.busca_casos_municipio("vILA pavao").casosConfirmados == 0)
     assert(relatorio.busca_casos_municipio("vILA pavao").obitos == 0)
 
 
